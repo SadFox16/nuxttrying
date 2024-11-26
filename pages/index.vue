cb7aea3cca76d514a5b26ca4dfc939668e553bf0
Oleg <oleg.s.kerimov@yandex.com>
2024-11-26 10:42:37 +0300

    Learn composables

--- pages/index.vue
+++ pages/index.vue
@@ -3,13 +3,20 @@
         <!-- <Alert/>
         <ProfileHeaderAvatar/> -->
         <!-- <Profile/> -->
-        Main page
+        Main page(pages)
         <IconsBell/>
-        <img src="../assets/1.webp" alt="image1"/>
+        <!-- <img src="../assets/1.webp" alt="image1"/>
         <img src="../assets/2.jpg" alt="image2"/>
         <img src="@/assets/1.webp" alt="image1"/>
         <img src="@/assets/2.jpg" alt="image2"/>
         <img src="/1.webp" alt="image1"/>
-        <img src="/2.jpg" alt="image2"/>
+        <img src="/2.jpg" alt="image2"/> -->
     </div>
 </template>
+
+<script>
+    // const sayHello = () => console.log('Hello')
+    // sayHello()
+    const { sayHello } = useUtils()
+    sayHello()
+</script>
---
+++ pages/profile.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Profile(pages)</h1>
+  </div>
+</template>
+
+<script setup>
+    // const sayHello = () => console.log('Goodbye')
+    // sayHello()
+    const { sayHello } = useUtils()
+    sayHello()
+</script>
+
+<style>
+
+</style>
